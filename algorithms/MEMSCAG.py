ab4bd71c8cf2c99d612ecfc9cd7b44d8a66ce93e
unbohn <nbohn.1988@googlemail.com>
2024-11-26 20:20:11 -0800

    Added header to MEMSCAG.py.

--- algorithms/MEMSCAG.py
+++ algorithms/MEMSCAG.py
@@ -1,3 +1,30 @@
+#! /usr/bin/env python
+#
+#  Copyright 2024 California Institute of Technology
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+# MEMSCAG: Multiple Endmember Snow-Covered Area and Grain Size
+#          Painter et al. (2003): https://www.sciencedirect.com/science/article/abs/pii/S0034425702001876)
+# Authors: Niklas Bohn, urs.n.bohn@jpl.nasa.gov
+#          Thomas H. Painter, tpainter@jifresse.ucla.edu (original c programming language version of MEMSCAG)
+#
+# Note: This Python version that fully independent of the original source code.
+#
+# Furthermore, this version is currently tailored to EMIT data
+# and will undergo significant updates in the future to become instrument agnostic.
+#
+
 from itertools import product
 import logging
 import multiprocessing
